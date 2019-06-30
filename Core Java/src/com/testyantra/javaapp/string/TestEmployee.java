21855349d1c3debaedb017a740e71a94473e36b7
NagarajPatilM <51954942+NagarajPatilM@users.noreply.github.com>
2019-06-30 15:40:31 +0530

    Delete TestEmployee.java

--- Core Java/src/com/testyantra/javaapp/string/TestEmployee.java
+++
@@ -1,9 +0,0 @@
-package com.testyantra.javaapp.string;
-
-public class TestEmployee {
-
-	public static void main(String[] args) {
-		ImmutableEmployee i=new ImmutableEmployee("nag",23,5.8,'M',true);
-	}
-
-}
